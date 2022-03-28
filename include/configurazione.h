3ed0c22ed70918419965df715a50c6a86f61289e
Gabriele <gabry96best@gmail.com>
2022-03-28 15:46:36 +0200

    costanti aggiornate

--- include/configurazione.h
+++ include/configurazione.h
@@ -1,3 +1,28 @@
+//configurazione hardware stepper driver
+#define MOT_1_STEP_PIN 2
+#define MOT_1_DIR_PIN 3
+#define MOT_1_EN_PIN 
+
+#define MOT_2_STEP_PIN 4
+#define MOT_2_DIR_PIN 5
+#define MOT_3_EN_PIN
+
+#define MOT_3_STEP_PIN 6
+#define MOT_3_DIR_PIN 7
+#define MOT_3_EN_PIN
+
+//configurazione sensori
 #define MOT_1_ENDSTOP_PIN 8
 #define MOT_2_ENDSTOP_PIN 9
 #define MOT_3_ENDSTOP_PIN 10
+
+//configurazione misure di sistema
+int MOT_1_MAX_MM = 0;
+int MOT_2_MAX_MM = 0;
+int MOT_3_MAX_MM = 0;
+int MOT_1_STEPS_PER_MM = 0;
+int MOT_2_STEPS_PER_MM = 0;
+int MOT_3_STEPS_PER_MM = 0;
+
+
+//configurazione
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@ namespace teensydmx = ::qindesign::teensydmx;
 void mot1endstop();
 void mot2endstop();
 void mot3endstop();
+void run();
 
 // Create the DMX receiver on Serial1.
 teensydmx::Receiver dmxRx{Serial1};
@@ -16,29 +17,15 @@ FlexyStepper motor3; // STEP pin: 6, DIR pin: 7
 void setup()
 {
 
-
-    pinMode(MOT_1_ENDSTOP_PIN, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(MOT_1_ENDSTOP_PIN), mot1endstop, RISING);
-
-    pinMode(MOT_2_ENDSTOP_PIN, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(MOT_3_ENDSTOP_PIN), mot2endstop, RISING);
- 
-    pinMode(MOT_3_ENDSTOP_PIN, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(MOT_3_ENDSTOP_PIN), mot3endstop, RISING);
-
-
     Serial.begin(115200);
-    motor1.connectToPins(2, 3);
-    motor2.connectToPins(4, 5);
-    motor3.connectToPins(6, 7);
+    motor1.connectToPins(MOT_1_STEP_PIN, MOT_1_DIR_PIN);
+    motor2.connectToPins(MOT_2_STEP_PIN, MOT_2_DIR_PIN);
+    motor3.connectToPins(MOT_3_STEP_PIN, MOT_3_DIR_PIN);
     dmxRx.begin();
-
     motor1.setSpeedInStepsPerSecond(10000000000);
     motor1.setAccelerationInStepsPerSecondPerSecond(20000);
-
     motor2.setSpeedInStepsPerSecond(10000000000);
     motor2.setAccelerationInStepsPerSecondPerSecond(20000);
-
     motor3.setSpeedInStepsPerSecond(10000000000);
     motor3.setAccelerationInStepsPerSecondPerSecond(20000);
 }
@@ -60,7 +47,7 @@ void loop()
     motor1.processMovement();
     motor2.processMovement();
     motor3.processMovement();
-}
+    }
 
 void mot1endstop() {}
 void mot2endstop() {}
