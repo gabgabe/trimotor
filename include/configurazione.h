ea78de0a05e889f3ba7f181581c4f243abe8c920
Gabriele <gabry96best@gmail.com>
2022-03-29 21:12:40 +0200

    beta 1.0.0

--- include/configurazione.h
+++ include/configurazione.h
@@ -1,4 +1,4 @@
-#define SVILUPPO // PROTOTYPE per pcb Delio || SVILUPPO per default
+#define PROTOTYPE // PROTOTYPE per pcb Delio || SVILUPPO per default
 
 #ifdef PROTOTYPE
 // configurazione IO
@@ -14,10 +14,10 @@
 #define MOT_3_DIR_PIN 30
 #define MOT_3_EN_PIN 28
 
-#define UP_ENDSTOP_PIN 4
-#define MID_UP_ENDSTOP_PIN 3
-#define MID_DOWN_ENDSTOP_PIN 2
-#define DOWN_ENDSTOP_PIN 1
+#define ENDSTOP_UP_PIN 1
+#define ENDSTOP_MID_UP_PIN 2
+#define ENDSTOP_MID_DOWN_PIN 3
+#define ENDSTOP_DOWN_PIN 4
 #endif
 #ifdef SVILUPPO
 // configurazione IO
@@ -43,15 +43,24 @@
 #define dmxStartChannel 1
 
 // configurazione stepper
-#define MOT_1_STEPS_PER_MM 50.0 // step/mm
-#define MOT_2_STEPS_PER_MM 50.0 // step/mm
-#define MOT_3_STEPS_PER_MM 50.0 // step/mm
-#define MOT_1_SPEED 5000.0      // mm/s
-#define MOT_2_SPEED 5000.0      // mm/s
-#define MOT_3_SPEED 5000.0      // mm/s
-#define MOT_1_ACCEL 500.0       // mm/s2
-#define MOT_2_ACCEL 500.0       // mm/s2
-#define MOT_3_ACCEL 500.0       // mm/s2
+#define MOT_1_STEPS_PER_MM 80.0 // step/mm
+#define MOT_2_STEPS_PER_MM 80.0 // step/mm
+#define MOT_3_STEPS_PER_MM 80.0 // step/mm
+#define MOT_1_SPEED 100.0      // mm/s
+#define MOT_2_SPEED 100.0      // mm/s
+#define MOT_3_SPEED 100.0      // mm/s
+#define MOT_HOMING_SPEED 50.0      // mm/s
+
+#define MOT_1_ACCEL 100.0       // mm/s2
+#define MOT_2_ACCEL 100.0       // mm/s2
+#define MOT_3_ACCEL 100.0       // mm/s2
+#define MOT_HOMING_ACCEL 100.0       // mm/s2
+#define MOT_HOMING_MAX_DISTANCE 3000 //mm
+#define MOT_1_DIRECTION -1;
+#define MOT_2_DIRECTION -1;
+#define MOT_3_DIRECTION -1;
+
+
 
 // sistema
 uint16_t A; // variabile interna
@@ -63,12 +72,13 @@ uint16_t pC; // variabile interna
 uint16_t A_MIN = 0;
 uint16_t B_MIN = 0;
 uint16_t C_MIN = 0;
-uint16_t A_MAX = 100;
-uint16_t B_MAX = 100;
-uint16_t C_MAX = 100;
+uint16_t A_MAX = 1800;
+uint16_t B_MAX = 1800;
+uint16_t C_MAX = 1800;
 uint16_t UNDER_A_OFFSET = 0;   // Gap aggiuntivo tra carrello inferiore e pavimento
 uint16_t BETWEEN_B_OFFSET = 0; // gap aggiuntivo tra carrello centrale e i due esterni
 uint16_t OVER_C_OFFSET = 0;    // gap aggiuntivo tra carrello superiore e soffitto
-long initial_homing = -1;
+long initial_homing = 1;
 int move_finished = 1;
-boolean homingFinished = false;
+boolean system_ready = false;
+boolean motors_initialized = false;
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 #include <FlexyStepper.h>
 #include <cstring>
 #include <TeensyDMX.h>
@@ -13,66 +11,64 @@ void mot4endstop();
 void run();             // stato di run su dmx, equivale all'on-air di uno studio
 void moveMotors();      // processa i movimenti dei 3 motori
 void processMeasures(); // calcola lo spazio possibile e
+
 #ifdef PROTOTYPE
 teensydmx::Receiver dmxRx{Serial3}; // Create the DMX receiver on Serial1.
 #endif
 #ifdef SVILUPPO
 teensydmx::Receiver dmxRx{Serial1}; // Create the DMX receiver on Serial1.
 #endif
-
 FlexyStepper motor1;
 FlexyStepper motor2;
 FlexyStepper motor3;
 
-boolean homing(FlexyStepper motorToHome, int endPin){
+boolean homing(FlexyStepper motorToHome, int endPin, int8_t direction)
+{
+    motorToHome.setAccelerationInMillimetersPerSecondPerSecond(MOT_HOMING_ACCEL);
+    motorToHome.setSpeedInMillimetersPerSecond(MOT_HOMING_SPEED);
+    while (!digitalRead(endPin)) // muovi verso endstop e testa while LOW
+    {
+        motorToHome.setTargetPositionInMillimeters(-MOT_HOMING_MAX_DISTANCE * direction);
+        motorToHome.processMovement();
+    }
+    motorToHome.setAccelerationInMillimetersPerSecondPerSecond(MOT_HOMING_ACCEL / 2);
+    motorToHome.setSpeedInMillimetersPerSecond(MOT_HOMING_SPEED / 8);
+    while (digitalRead(endPin)) // muovi verso su e testa while HIGH  && ((millis() - lastMillis) > 50)
+    {                           //   Serial.println("mi allontando da endstop");
+        motorToHome.setTargetPositionInMillimeters(MOT_HOMING_MAX_DISTANCE * direction);
+        motorToHome.processMovement();
+    }
 
-  motorToHome.setAccelerationInStepsPerSecondPerSecond(100000);
-  motorToHome.setSpeedInStepsPerSecond(1000);
-  while(digitalRead(endPin)){
-    motorToHome.moveRelativeInSteps(initial_homing);
-    initial_homing--;
-    motorToHome.processMovement();
-    delay(5);
-  }
-  motorToHome.setCurrentPositionInSteps(0);
-  initial_homing = 1;
+    motorToHome.setCurrentPositionInMillimeters(0);
+    Serial.println("Homing Completed");
+    Serial.println("");
 
-  while (!digitalRead(endPin)) { // Make the Stepper move CW until the switch is deactivated
-    motorToHome.setAccelerationInStepsPerSecondPerSecond(10000);
-    motorToHome.setSpeedInStepsPerSecond(1000);
-    motorToHome.moveRelativeInSteps(initial_homing);  
-    initial_homing++;
-    motorToHome.processMovement();
-    delay(5);  
-  }
-
-  motorToHome.setCurrentPositionInSteps(0);
-  return true;
-  
-  Serial.println("Homing Completed");
-  Serial.println("");
+    return true;
 }
-
-
 void setup()
 {
+    dmxRx.begin();
     Serial.begin(115200);
+    dmxRx.begin();
     motor1.connectToPins(MOT_1_STEP_PIN, MOT_1_DIR_PIN);
     motor2.connectToPins(MOT_2_STEP_PIN, MOT_2_DIR_PIN);
     motor3.connectToPins(MOT_3_STEP_PIN, MOT_3_DIR_PIN);
-    dmxRx.begin();
     motor1.setStepsPerMillimeter(MOT_1_STEPS_PER_MM);
     motor2.setStepsPerMillimeter(MOT_2_STEPS_PER_MM);
     motor3.setStepsPerMillimeter(MOT_3_STEPS_PER_MM);
-
     motor1.setSpeedInMillimetersPerSecond(MOT_1_SPEED);
     motor2.setSpeedInMillimetersPerSecond(MOT_2_SPEED);
     motor3.setSpeedInMillimetersPerSecond(MOT_3_SPEED);
-
     motor1.setAccelerationInMillimetersPerSecondPerSecond(MOT_1_ACCEL);
     motor2.setAccelerationInMillimetersPerSecondPerSecond(MOT_2_ACCEL);
     motor3.setAccelerationInMillimetersPerSecondPerSecond(MOT_3_ACCEL);
-    systemInitialization();
+    pinMode(ENDSTOP_UP_PIN, INPUT_PULLUP);
+    pinMode(ENDSTOP_MID_UP_PIN, INPUT_PULLUP);
+    pinMode(ENDSTOP_MID_DOWN_PIN, INPUT_PULLUP);
+    pinMode(ENDSTOP_DOWN_PIN, INPUT_PULLUP);
+    system_ready = false;
+    motors_initialized = false;
+    delay(1000);
 }
 void loop()
 {
@@ -96,14 +92,17 @@ void moveMotors()
 void processMeasures()
 {
     A = dmxRx.get16Bit(dmxStartChannel);
-    //    Serial.print("    CH1 :  ");
-    //    Serial.print(A);
     B = dmxRx.get16Bit(dmxStartChannel + 2);
-    //    Serial.print("    CH3 :  ");
-    //    Serial.print(B);
     C = dmxRx.get16Bit(dmxStartChannel + 4);
-    //    Serial.print("    CH5 :  ");
-    //    Serial.println(C);
+
+#ifdef DEBUG_ON
+    Serial.print("    CH1 :  ");
+    Serial.print(A);
+    Serial.print("    CH3 :  ");
+    Serial.print(B);
+    Serial.print("    CH5 :  ");
+    Serial.println(C);
+#endif
 
     A = map(A, 0, 65535, A_MIN, A_MAX);
     B = map(B, 0, 65535, B_MIN, B_MAX);
@@ -112,18 +111,32 @@ void processMeasures()
     pA = constrain(A, A_MIN, pB);
     pB = constrain(B, pA, pC);
     pC = constrain(C, pB, C_MAX);
-/*
+
+#ifdef DEBUG_ON
     Serial.print("    CH1 :  ");
     Serial.print(pA);
     Serial.print("    CH3 :  ");
     Serial.print(pB);
     Serial.print("    CH5 :  ");
     Serial.println(pC);
-    */
+#endif
 }
-void mot1endstop() {}
-void mot2endstop() {}
-void mot3endstop() {}
-void mot4endstop() {}
-void systemInitialization() {}
->>>>>>> gab
+void systemInitialization()
+{
+    delay(1000);
+    if (homing(motor1, ENDSTOP_DOWN_PIN, MOT_1_DIRECTION))
+    {
+        delay(500);
+        if (homing(motor2, ENDSTOP_MID_DOWN_PIN, MOT_2_DIRECTION))
+        {
+            delay(500);
+            if (homing(motor3, ENDSTOP_MID_UP_PIN, MOT_3_DIRECTION))
+            {
+                motors_initialized = true;
+                delay(500);
+            }
+        }
+    }
+    else
+        break;
+}
